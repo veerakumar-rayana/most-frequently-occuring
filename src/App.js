5605d21a1cdce6d9eeae3dc45092869646aa5861
veerakumar-rayana <62836860+veerakumar-rayana@users.noreply.github.com>
2021-05-17 14:02:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import WordCount from "./components/WordCount";
+
+function App() {
+  return (
+    <div className="App">
+      <WordCount />
+    </div>
+  );
+}
+
+export default App;
